77b91afdfabb6330f3c10d6214c86426fcf13fc5
arash ajam <arashajam@arashs-MacBook-Pro.local>
2021-10-27 16:53:43 -0600

    Apollo auth added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
 import { useQuery, gql } from '@apollo/client';
+import { setContext } from "@apollo/client/link/context";
 import Users from './components/Users';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Landing from './components/Landing';
-const client = new ApolloClient({
-  uri: "http://localhost:4000", // specifies the URL of our GraphQL server
-  cache: new InMemoryCache() // is an instance of InMemoryCache, which Apollo Client uses to cache query results after fetching them.
+
+const httpLink = new HttpLink({ uri: "http://localhost:4000" })
+
+const authLink = setContext(async (req, { headers }) => { // sets context for a request to be used by resolvers for auth
+	const token = localStorage.getItem("token")
+
+	return {
+		...headers,
+		headers: {
+			Authorization: token ? `Bearer ${token}` : null
+		}
+	}
 })
+
+const link = authLink.concat(httpLink as any) //sends along authorization header to every http request
+const client = new ApolloClient({
+	link: link as any, // specifies the URL of our GraphQL server
+  cache: new InMemoryCache() // is an instance of InMemoryCache, which Apollo Client uses to cache query results after fetching them.
+
+})
+
 
 function App() {
 
