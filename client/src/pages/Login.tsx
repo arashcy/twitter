b3ee578a3f836a7c468c4e8f90342c75e0288dd9
arash ajam <arashajam@arashs-MacBook-Pro.local>
2021-11-01 17:41:10 -0600

    add login page

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,78 @@
+import { gql, useMutation } from "@apollo/client";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import * as Yup from "yup";
+import TwitterLogo from "../styles/assets/twitter-logo.png";
+
+const LOGIN_MUTATION = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
+
+interface LoginValues {
+  email: string;
+  password: string;
+}
+
+function Login() {
+    const history = useHistory();
+    const [login, { data }] = useMutation(LOGIN_MUTATION);
+    const initialValues: LoginValues = {
+    email: "",
+    password: "",
+    };
+
+    if(data?.error) {
+        return <p>{data.error.message}</p>
+    }
+
+    const validationSchema = Yup.object({
+    email: Yup.string()
+        .email("Invalid email address")
+        .required("Email Required"),
+    password: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .required("Password Required"),
+    });
+
+    return (
+    <div className="container">
+        <h3>Log in to Twitter Clone</h3>
+        <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={async (values, { setSubmitting }) => {
+            setSubmitting(true);
+            const response = await login({
+            variables: values,
+            });
+            localStorage.setItem("token", response.data.login.token);
+            setSubmitting(false);
+            history.push("/users");
+        }}
+        >
+        <Form>
+            <Field name="email" type="text" placeholder="Email" />
+            <ErrorMessage name="email" component={"div"} />
+
+            <Field name="password" type="password" placeholder="Password" />
+            <ErrorMessage name="password" component={"div"} />
+
+            <button type="submit" className="login-button">
+            <span>Login</span>
+            </button>
+        </Form>
+        </Formik>
+        <div className="register">
+        <h4>Don't have an account?</h4>
+        <Link to="/signup">Sign up</Link>
+        </div>
+    </div>
+    );
+    }
+
+export default Login;
